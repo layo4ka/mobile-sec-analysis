eb2f0d97ad4f69d51796061f39ab4164bb6787b8
VanoMV <56218988+JustAuser677@users.noreply.github.com>
2025-07-14 13:13:35 +0300

    Починил подключение к MobSF
    Теперь requests может спокойно подключаться к MobSF и отправлять на него POST и GET запросы

--- mobile_security_service/file/analysis.py
+++ mobile_security_service/file/analysis.py
@@ -4,18 +4,18 @@ import json
 MOBSF_URL = "http://mobsf:8000"
 API_KEY = "abf1e023d641ec0f8d1c5d1e723fb2e21d324ece74afd78d9d9bd354a110dc81"
 
-def upload_app(file_instance):
+def upload_app(file_instance, path):
     headers = {"Authorization": API_KEY}
     uploadURL = f"{MOBSF_URL}/api/v1/upload"
-    toPost = {"file":file_instance}
-    response = requests.post(uploadURL, files=toPost, headers=headers)
+    toPost = {"file":(path, file_instance, 'application/octet-stream')}
+    response = requests.post(uploadURL, files=toPost, headers=headers, timeout=20)
 
     if response.status_code == 200:
         result = response.json()
         file_hash = result.get("hash")
         return file_hash
     else:
-        return f"Ошибка {response.status_code}"
+        return f"Ошибка {response.status_code}; {response.json}; {response.text}"
 
 def static_analysis(file_hash):
     headers = {"Authorization": API_KEY}
@@ -23,6 +23,30 @@ def static_analysis(file_hash):
     toPost = {"hash":file_hash}
     response = requests.post(uploadURL, data=toPost, headers=headers)
     if response.status_code == 200:
-        return response.json
+        return "Done"
     else:
-        return "Ошибка"
+        return f"Ошибка: {response.json}; {response.text}"
+    
+def get_results_report(status, file_hash):
+    if(status == "Done"):
+
+        headers = {"Authorization": API_KEY}
+        uploadURL = f"{MOBSF_URL}/api/v1/report_json"
+        toPost = {"hash":file_hash}
+        response = requests.post(uploadURL, data=toPost, headers=headers)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return f"Ошибка: {response.status_code}; {response.text}"
+    else:
+        return status
+def get_scan_results(status, file_hash):
+    if(status=="Done"):
+        headers = {"Authorization": API_KEY}
+        uploadURL = f"{MOBSF_URL}/api/v1/search"
+        toPost = {"hash":file_hash}
+        response = requests.post(uploadURL, data=toPost, headers=headers)
+        if response.status_code == 200:
+            return json.dumps(response.json, indent=4, ensure_ascii=False)
+    else:
+        return f"Ошибка: {response.status_code}; {response.text}"
--- mobile_security_service/file/task.py
+++ mobile_security_service/file/task.py
@@ -1,6 +1,6 @@
 from celery import shared_task
 import celery.worker
-from .analysis import upload_app
+from .analysis import upload_app, static_analysis, get_results_report, get_scan_results
 from .models import UploadFile
 from celery import Celery
 from django.apps import apps
@@ -10,8 +10,13 @@ from celery.utils.log import get_task_logger
 
 @shared_task()
 def process_file_async(analysisFilePath):
+    result = ''
     file = default_storage.open(analysisFilePath, "rb")
-    result = upload_app(file)
+    file_hash = upload_app(file, analysisFilePath)
+    analysis_status = static_analysis(file_hash)
+    report = get_results_report(analysis_status, file_hash)
+    scan_results=get_scan_results(analysis_status, file_hash)
+    result+=report
     return result
         
     
