2f180eb6476fed64f4768e507c098adcaace55e3
layo4ka <123590277+layo4ka@users.noreply.github.com>
2025-07-15 12:31:29 +0300

    Update task.py

--- mobile_security_service/file/task.py
+++ mobile_security_service/file/task.py
@@ -1,6 +1,6 @@
 from celery import shared_task
 import celery.worker
-from .analysis import upload_app, static_analysis, get_results_report, get_scan_results
+from .analysis import upload_app, static_analysis, get_results_report
 from .models import UploadFile
 from celery import Celery
 from django.apps import apps
@@ -28,7 +28,7 @@ def process_file_async(analysisFilePath):
     file_hash = upload_app(file, analysisFilePath)
     analysis_status = static_analysis(file_hash)
     report = get_results_report(analysis_status, file_hash)
-    scan_results=get_scan_results(analysis_status, file_hash)
+   
 
     result+=report
     return f"Длина JSON результата (пока нормально отображение не сделал): {len(result)}"
