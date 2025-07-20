136e08f8bac044ccf391848183445deccf7c09cd
layo4ka <123590277+layo4ka@users.noreply.github.com>
2025-07-20 10:31:39 +0300

    добавляем/проверяем строки:
    Настроить MEDIA_URL, MEDIA_ROOT и ALLOWED_HOSTS

--- mobile_security_service/mobile_security_service/settings.py
+++ mobile_security_service/mobile_security_service/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-!jbtz^4xv76sk*5n%fgu$uz)yu!qss1pu+=m9p0n6z9o@ur_*y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['host.docker.internal', 'localhost']
+ALLOWED_HOSTS = ['host.docker.internal', 'localhost', "127.0.0.1"]
 
 
 # Application definition
@@ -132,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 MEDIA_URL= "/media/"
-MEDIA_ROOT=BASE_DIR/"media"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -151,3 +152,4 @@ CELERY_RESULT_EXTENDED = True
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 
 MOBSF_URL = "http://mobsf:8000"
+MOBSF_API_KEY='abf1e023d641ec0f8d1c5d1e723fb2e21d324ece74afd78d9d9bd354a110dc81'
